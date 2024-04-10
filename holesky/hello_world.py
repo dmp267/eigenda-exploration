c706d49b5707fa0e5475efae57dfe8e9e42b2008
Daniel Parangi <danielparangi@Daniels-MBP.attlocal.net>
2024-04-10 12:57:05 -0400

    update hello world

---
+++ holesky/hello_world.py
@@ -0,0 +1,32 @@
+import grpc
+import subprocess
+
+from proto.disperser.disperser_pb2 import DisperseBlobRequest, DisperseBlobResponse, GetBlobStatusRequest, GetBlobStatusResponse
+from proto.disperser.disperser_pb2_grpc import DisperserStub
+
+
+def encode_data(data):
+    result = subprocess.run(["tools/kzgpad/bin/kzgpad", "-e", data], capture_output=True, text=True)
+    return result.stdout.strip()
+    
+
+def main(data):
+    channel = grpc.secure_channel("disperser-holesky.eigenda.xyz:443", grpc.ssl_channel_credentials())
+    stub = DisperserStub(channel)
+
+    encoded_data = encode_data(data)   
+    request = DisperseBlobRequest(data=encoded_data)
+
+    response = stub.DisperseBlob(request)
+    print(response)
+
+    try:
+        request = GetBlobStatusRequest(request_id=response.request_id)
+        response = stub.GetBlobStatus(request)
+        print(response)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main("hello world")
